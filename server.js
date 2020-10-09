1e05a2ea44aa91a140b22632c4db172b34e360ff
vinay.maurya-au7 <vinay.shermauryan@gmail.com>
2020-10-09 17:02:38 +0530

    Final submission

--- server.js
+++ server.js
@@ -1,23 +1,81 @@
 const express = require("express");
-const app = express();
-const connectToDatabase = require("./config/connectToDatabase");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+require("dotenv").config();
 const cors = require("cors");
 
-//Function that connects express app to database
-connectToDatabase();
+// our routes
+const productRouter = require("./routes/api/product");
+const categoryRouter = require("./routes/api/category");
+const usersRouter = require("./routes/api/users");
+const cartRouter = require("./routes/api/cart");
+const wishlistRouter = require("./routes/api/wishlist");
+const addressRouter = require("./routes/api/address");
+const orderRouter = require("./routes/api/order");
+const permissionsRouter = require("./routes/api/permission");
 
-//We prevent from cors policy warning
+const app = express();
+
+// for body-parser middleware
+app.use(express.json());
+
+//cors middleware
 app.use(cors());
 
-//Allows us to use body json thing to create posts
-app.use(express.json({ extended: false }));
+// morgan logger for dev
+app.use(logger("dev"));
 
-//Routes
-app.use("/api/posts", require("./routes/posts.js"));
-app.use("/api/users", require("./routes/users.js"));
+//make our upload an accesable folder
+app.use("/uploads", express.static("uploads"));
 
-//We specified variable on which port our app will run (depends if heroku will give us port or we specify on 5000)
-let PORT = process.env.PORT || 5000;
+// Database uri
+const dbURI = process.env.DB_URI;
 
-//Method to specify on which port we want our app to be with callback function to see if method works
-app.listen(PORT, () => console.log(`---Server is on port: ${PORT}---`));
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+//test database connection
+let db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("mongoDB is connected");
+});
+
+// Set up our main routes
+app.use("/api/product", productRouter);
+app.use("/api/category", categoryRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/wishlist", wishlistRouter);
+app.use("/api/address", addressRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/permissions", permissionsRouter);
+
+
+// if the request passes all the middleware without a response
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+// for general error handling
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    message: error.response
+  });
+});
+
+// heroku create $APP_NAME --buildpack mars/create-react-app
+// App's connection port
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
+
+
